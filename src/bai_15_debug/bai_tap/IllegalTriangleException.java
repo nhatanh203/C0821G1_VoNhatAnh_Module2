0df71de644c1c3f639da8c8eb4f10561bd5f9377
nhatanh203 <vonhatanh462003@gmail.com>
2021-10-26 21:39:38 +0700

    baitapdebug

---
+++ src/bai_15_debug/bai_tap/IllegalTriangleException.java
@@ -0,0 +1,9 @@
+package bai_15_debug.bai_tap;
+
+import java.util.Scanner;
+
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException(String message) {
+        super(message);
+    }
+}
